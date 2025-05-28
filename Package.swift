a305d730241aafb2ef9582e2237481fd609a235c
Kashif Hussain <kashif.hussain@enthral.ai>
2025-05-28 12:21:40 +0530

    fixes in package resolution

--- Package.swift
+++ Package.swift
@@ -10,17 +10,16 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VimeoRestrictedPlayer",
             targets: ["VimeoRestrictedPlayer"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VimeoRestrictedPlayer"
-        ),
+            name: "VimeoRestrictedPlayer"),
         .testTarget(
             name: "VimeoRestrictedPlayerTests",
             dependencies: ["VimeoRestrictedPlayer"]
